3e2b343ffa19fbfeac3df9d10d6d5c405ad55237
TTatchell <tim.tatchell24@gmail.com>
2021-11-30 20:45:36 +1100

    Added redux reducer for screen state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import logo from "./logo.svg";
 import "./Styles/App.css";
 import "bulma/css/bulma.min.css";
 
+// Redux
+import { useSelector, useDispatch } from 'react-redux'
+import { home, products, about, contact } from './Features/screen/screenSlice'
+
+// Components
 import Header from './Components/Header';
 import HomePage from './Components/HomePage';
+import Products from './Components/Products';
 import Footer from './Components/Footer';
 
 function App() {
+
+  const screen = useSelector((state) => state.screen.value)
+  const dispatch = useDispatch()
+
   return (
-    <div classname="main">
+    <div classname='main'>
       < Header />
-      < HomePage />
+      {screen}
       < Footer />
     </div>
-  );
+  )
 }
 
 export default App;
