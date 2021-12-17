40b6fd0e5794d33a2244e1cb11671c7facac888b
TTatchell <tim.tatchell24@gmail.com>
2021-12-17 16:45:26 +1100

    Created separate loading bar component

---
+++ src/Components/LoadingBar.js
@@ -0,0 +1,7 @@
+export const LoadingBar = () => {
+    return (
+        <section className="section">
+            <progress class="progress is-large is-info" max="100">60%</progress>
+        </section>
+    );
+};
