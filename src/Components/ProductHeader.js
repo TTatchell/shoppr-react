9a0f765511f7662960e20d8776e7e7375ec7c105
TTatchell <tim.tatchell24@gmail.com>
2021-12-17 16:59:16 +1100

    Created ProductHeader component

---
+++ src/Components/ProductHeader.js
@@ -0,0 +1,11 @@
+export const ProductHeader = (props) => {
+    return (
+        <section className="hero is-info is-medium is-bold">
+            <div className="hero-body">
+                <div className="container has-text-centered">
+                    <h1 className="title">Products</h1>
+                </div>
+            </div>
+        </section>
+    );
+};
